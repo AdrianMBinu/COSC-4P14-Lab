3549977044d3bfb862bcb5905e58461d2738a13f
AdrianMBinu <146862341+AdrianMBinu@users.noreply.github.com>
2023-10-11 19:08:46 -0400

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -6,7 +6,17 @@ public class Client {
 
     public Client() {
         try {
+            System.out.println("client started");
             client = new Socket("localHost", 8080);
+
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("enter a string");
+            String str = userInput.readLine();
+            PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+            out.println(str);
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            System.out.println(in.readLine());
+            
             client.close();
         } catch (Exception ignored) {}
     }
